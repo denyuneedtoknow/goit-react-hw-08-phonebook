0f7067d2cfff318442f8fb00bae3d3862bdb95fc
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-11-30 16:26:57 +0200

    nav css

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 import { logoutUser } from "../../redux/Users/users-operations"
 import { useSelector } from "react-redux";
 import { isAuthorized } from '../../redux/Users/users-selectors'
+import { getUserName } from '../../redux/Users/users-selectors'
 import s from "./Navigation.module.css";
 
 
@@ -12,6 +13,8 @@ import s from "./Navigation.module.css";
 
 const Navigation = () => {
     const isAuth = useSelector(isAuthorized)
+    const username = useSelector(getUserName)
+
     const dispatch = useDispatch()
 
     const handleLogOut = () => {
@@ -22,14 +25,22 @@ const Navigation = () => {
         <div className={s.navigation}>
             {isAuth ?
                 <>
-                    <NavLink to="/" className={s.link} activeclassname={s.activeLink}>Home</NavLink>
-                    <NavLink to="/contacts" className={s.link} activeclassname={s.activeLink}>Contacts</NavLink>
-                    <button type="button" onClick={handleLogOut}>Log Out</button>
+                    <ul className={s.navMenu}>
+                        <li className={s.navMenuItems}><NavLink to="/contacts" className={s.link} activeclassname={s.activeLink}>Contacts</NavLink></li>
+                        <li className={s.navMenuItems}><NavLink to="/" className={s.link} activeclassname={s.activeLink}>Home</NavLink></li>
+                        <li className={s.navMenuItems}><p className={s.link}>Welcome, {username}</p></li>
+                        <li className={s.navMenuItems}><button type="button" className={s.addBtn} onClick={handleLogOut}>Log Out</button></li>
+                    </ul>
                 </> :
                 <>
-                    <NavLink to="/login" className={s.link} activeclassname={s.activeLink}>Log In </NavLink>
-                    <NavLink to="/register" className={s.link} activeclassname={s.activeLink}>Registration</NavLink></>}
+                    <ul className={s.navMenu}>
+                        <li className={s.navMenuItems}><NavLink to="/login" className={s.link} activeclassname={s.activeLink}>Log In </NavLink></li>
+                        <li className={s.navMenuItems}><NavLink to="/register" className={s.link} activeclassname={s.activeLink}>Registration</NavLink></li>
+                    </ul>
 
+
+                </>
+            }
 
         </div>
     );
