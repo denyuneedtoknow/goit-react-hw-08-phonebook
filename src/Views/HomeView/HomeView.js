eb99eb9a199caca0ccdbf35367da73d94f00c758
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-12-02 15:24:41 +0200

    misc add

--- src/Views/HomeView/HomeView.js
+++ src/Views/HomeView/HomeView.js
@@ -1,6 +1,18 @@
+import SimpleSnackbar from '../../Components/Miscellaneous/snackbar'
+
+
 const HomeView = () => {
+
+    const opener = () => {
+        console.log('open');
+    }
+
     return (
-        <h1>Home</h1>
+        <><h1>Home</h1>
+            {/* <button type='button' onClick={opener}>click</button> */}
+            <SimpleSnackbar props={opener} />
+        </>
+
     )
 }
 
